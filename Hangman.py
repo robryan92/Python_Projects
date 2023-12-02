06e6e695969d1804630f5855945a3a38bc2e8d64
robryan92 <rob.ryan36@yahoo.com>
2023-12-02 10:54:03 -0500

    Created new file for killer and survivor perks to import into main randomizer script

--- Hangman.py
+++ Hangman.py
@@ -4,7 +4,6 @@ from hangman_words import word_list
 
 print(logo)
 end_of_game = False
-# word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
