c68f37788201e93175c3dcd97dea2ee1135a75be
robryan92 <rob.ryan36@yahoo.com>
2023-12-13 18:12:13 -0500

    corrected inaccurate guess counting at end of game

--- Guessing_Game_Project.py
+++ Guessing_Game_Project.py
@@ -7,13 +7,14 @@ def retry():
     if retry.lower() == "y":
         os.system('cls')
         guessing_game()
-    else:
+    elif retry.lower() == "n":
         print("Goodbye!")
+        return
 def guessing_game():
     print(logo)
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
-    random_number = random.randint(0, 101)
+    random_number = random.randint(1, 100)
     difficulty = input("Choose a difficulty. Type 'easy or 'hard': ")
     guesses = ""
     if difficulty == "easy":
@@ -27,6 +28,7 @@ def guessing_game():
         guess = int(input("Make a guess: "))
         if guess == random_number:
             print(f"You got it!  The answer was {random_number}.")
+            guesses = 0
             retry()
         elif guess > random_number:
             guesses -= 1
